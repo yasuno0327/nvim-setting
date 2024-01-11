373e970dba2bde55977410aebe56a7a780d62846
yoshitomo.yasuno <yoshitomo.yasuno@aktsk.jp>
2024-01-11 15:24:39 +0900

    fix

--- init.lua
+++ init.lua
@@ -139,4 +139,7 @@ require("ibl").setup()
 
 require"toggleterm".setup {}
 
-require"session_manager".setup {}
+local config = require('session_manager.config')
+require"session_manager".setup {
+  autoload_mode = config.AutoloadMode.CurrentDir,
+}
