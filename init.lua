ae4ad06360ee17c7e9b7253fb9d683d58c6a2810
yasuno0327 <yasuno0327@gmail.com>
2024-10-09 12:03:45 +0900

    reload keymap

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
 require("config.lazy")
 require("config.autocmds")
+-- Can't load plugin api in lazy autoload
+-- So... Reload keymaps here
+require("config.keymaps")
 -- config/keymaps and config/options are automatically loaded before lazy.nvim startup
