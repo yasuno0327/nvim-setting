134d43fa3f3e03a918fed0c607f288b2c7455d30
yasuno0327 <yasuno0327@gmail.com>
2024-10-23 15:02:00 +0900

    add plugins of indent-rainbow and markdown preview

--- lua/plugins/indent-blank.lua
+++ lua/plugins/indent-blank.lua
@@ -1,3 +1,10 @@
 return {
-  { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		dependencies = "TheGLander/indent-rainbowline.nvim",
+		opts = function(_, opts)
+			return require("indent-rainbowline").make_opts(opts)
+		end,
+	},
 }
