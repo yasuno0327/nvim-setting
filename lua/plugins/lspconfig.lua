e9d7bad67a4410e8642b0776f3d5a42f531c9202
yasuno0327 <yasuno0327@gmail.com>
2025-04-11 23:08:27 +0900

    nvim-javaを導入

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -6,6 +6,9 @@ return {
 			{ "antosha417/nvim-lsp-file-operations", config = true },
 		},
 		config = function()
+			-- nvim-javaはlspconfigの前にセットアップする必要があるらしい？
+			-- ref: https://github.com/nvim-java/nvim-java?tab=readme-ov-file#hammer-how-to-install
+			require("java").setup()
 			local lspconfig = require("lspconfig")
 			lspconfig.dockerls.setup({})
 			lspconfig.dotls.setup({})
